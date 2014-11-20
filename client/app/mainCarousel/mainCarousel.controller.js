f429067c3e5bc14011e17dbcd1c62666eee1cee2
ginna-baker <ginna.baker@gmail.com>
2014-11-20 14:53:04 -0500

    slides redone for clarity and color

--- client/app/mainCarousel/mainCarousel.controller.js
+++ client/app/mainCarousel/mainCarousel.controller.js
@@ -8,15 +8,15 @@ angular.module('gnittyApp')
       //revisit newWidth
       var newWidth = 600 + slides.length;
       slides.push({
-        image: 'assets/images/sentiment_screenshot.jpg',
+        image: 'assets/images/sentiment_screenshot_cropped.jpg',
         text: 'How do I come across in email?'
       },
       {
-        image: 'assets/images/stacked_bar_screenshot.jpg',
+        image: 'assets/images/stacked_screenshot_cropped.jpg',
         text: 'How much email do I generate?'
       },
       {
-        image: 'assets/images/scatter_screenshot.jpg',
+        image: 'assets/images/scatter_screenshot_cropped.jpg',
         text: 'What time of day am I mmost productive?'
       });
     };
