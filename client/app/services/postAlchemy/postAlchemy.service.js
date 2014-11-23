323069078592ccd8669b699c60f1896417948c70
Gabriel Lebec <glebec@gmail.com>
2014-11-23 16:05:42 -0500

    slightly DRYer alchemy API

--- client/app/services/postAlchemy/postAlchemy.service.js
+++ client/app/services/postAlchemy/postAlchemy.service.js
@@ -9,17 +9,20 @@ angular.module('gnittyApp')
         '/api/alchemy/keywords',
         '/api/alchemy/concepts'
       ];
-      // map each API route to a promise for its response
+      // map each API call to a promise for its response.data
       alchemyCalls = alchemyCalls.map( function makeCall (url) {
-        return $http.post( url, {text: text, outputMode: 'json'} );
+        return $http.post( url, {text: text, outputMode: 'json'} )
+          .then( function received (response) {
+            return response.data;
+          });
       });
       // return a promise for one object representing all the data
       return $q.all( alchemyCalls ).then( function received (results) {
         $log.debug('Alchemy results: ', results);
         return {
-          sentiment : results[0].data,
-          keywords : results[1].data,
-          concepts : results[2].data
+          sentiment : results[0],
+          keywords : results[1],
+          concepts : results[2]
         };
       });
     };
