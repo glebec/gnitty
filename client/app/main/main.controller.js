36e64af82343165ad4946192be155349d52bbd8b
Gabriel Lebec <glebec@gmail.com>
2014-11-23 21:22:08 -0500

    editing main ctrl for legibility and removed unused twilio

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,27 +6,18 @@ angular.module('gnittyApp')
     // initialize google api in case already signed in, etc. TODO: fix this
     // gAPI.handleClientLoad();
 
-    // DEV TESTING ONLY, REMOVE BEFORE DEPLOYMENT
-    $scope.setLocal = function () {
-      emails.setLocal();
-      stats.setLocal();
-    };
-    $scope.getLocal = function () {
-      emails.getLocal();
-      stats.getLocal();
-    };
-
-    // Fetch button status
     $scope.fetchBtnText = 'Gnitify!';
+
     // fetch emails from Gmail API and store them in the email service
     $scope.fetch = function () {
-      $scope.clicked=true;
+
+      $scope.clicked = true;
       $scope.fetchBtnText = 'Authorizing…';
+
       gAPI.start().then( runFetchers );
 
       function runFetchers () {
         $scope.fetchBtnText = 'Fetching…';
-
         gAPI.collectEmails().then(
           function fetchSuccess ( emailData ) {
             $scope.fetchBtnText = 'Fetched!';
@@ -53,29 +44,23 @@ angular.module('gnittyApp')
             $scope.fetchBtnText = 'OOPS…';
           }
         );
-
       }
 
     };
 
-    // show stored data
+    // localStorage access for production
+    $scope.setLocal = function () {
+      emails.setLocal();
+      stats.setLocal();
+    };
+    $scope.getLocal = function () {
+      emails.getLocal();
+      stats.getLocal();
+    };
     $scope.showEmails = function () {
       $log.info('data stored in email service: ', emails.data);
       $log.info('dates length: ', emails.dateLengthSentBoolArr.length);
       $log.info('text length: ', emails.textArr.length);
-    };
-
-    $scope.link = 'http://www.ginnabaker.com';
-    $scope.clientObj = {};
-    $scope.clientObj.phoneNum;
-    $scope.mediaUrl = 'http://i495.photobucket.com/albums/rr313/trtla/ist2_1050220-red-crayon-heart.jpg';
-
-    $scope.sendMMS = function() {
-      $http.post('/api/twilio', {
-        body: 'From Ginna -- Just found out I wrote more words than Hemingway this year.  Want to try?: '+$scope.link,
-        to: '+1'+$scope.clientObj.phoneNum, //9179524491
-        mediaUrl: $scope.mediaUrl
-      });
     };
 
   });
