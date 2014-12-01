5fecfd23d2c40a625d0508090c567067ae8c1a50
ginna-baker <ginna.baker@gmail.com>
2014-11-30 21:20:29 -0500

    slide text finalized

--- client/app/mainCarousel/mainCarousel.controller.js
+++ client/app/mainCarousel/mainCarousel.controller.js
@@ -8,16 +8,28 @@ angular.module('gnittyApp')
       //revisit newWidth
       var newWidth = 600 + slides.length;
       slides.push({
-        image: 'assets/images/sentiment_screenshot_cropped.jpg',
-        text: 'How do I come across in email?'
+        header: 'How do I come across in email?',
+        text: ''
       },
       {
-        image: 'assets/images/stacked_screenshot_cropped.jpg',
-        text: 'How much email do I generate?'
+        header: 'What time of day am I most productive?',
+        text: ''
       },
       {
-        image: 'assets/images/scatter_screenshot_cropped.jpg',
-        text: 'What time of day am I mmost productive?'
+        header: 'Do I get more email than the average person?',
+        text: ''
+      },
+      {
+        header: 'Pick out patterns, understand your inbox. Gnitty gets down to the nitty-gritty of your last 1,000 Gmail messages.',
+        text: ''
+      },
+      {
+        header: 'How does it work?',
+        text: 'Gnitty analyzes your mail securely in the browser, saving nothing. We partner with Alchemy to glean your keywords. Authenticated through Google, meaning you remain in control. '
+      },
+      {
+        header: 'How do I get started?',
+        text: 'Click the GNITIFY button and see results in 10 seconds or less.'
       });
     };
     $scope.addSlide();
